0da79a0e4590bdb3bb3ff1b1ab5843f91697116e
hrishabd7 <132309782+hrishabd7@users.noreply.github.com>
2025-05-04 22:51:04 -0500

    Add files via upload

--- msdap_uvm_framework-2/tb_top.sv
+++ msdap_uvm_framework-2/tb_top.sv
@@ -7,12 +7,19 @@ module tb_top;
 
   // Instantiate DUT here (replace MSDAP with your DUT name)
   MSDAP dut (
-    .clk(clk),
-    .rst_n(rst_n),
-    .in_data(intf.in_data),
-    .in_valid(intf.in_valid),
-    .out_data(intf.out_data),
-    .out_valid(intf.out_valid)
+  // Instantiate DUT here (replace MSDAP with your DUT name)
+MSDAP dut (
+    .Dclk(msdap_if_inst.Dclk),
+    .Sclk(msdap_if_inst.Sclk),
+    .Reset_n(msdap_if_inst.Reset_n),
+    .Frame(msdap_if_inst.Frame),
+    .Start(msdap_if_inst.Start),
+    .InputL(msdap_if_inst.InputL),
+    .InputR(msdap_if_inst.InputR),
+    .InReady(msdap_if_inst.InReady),
+    .OutReady(msdap_if_inst.OutReady),
+    .OutputL(msdap_if_inst.OutputL),
+    .OutputR(msdap_if_inst.OutputR)
   );
 
   // Clock generation
